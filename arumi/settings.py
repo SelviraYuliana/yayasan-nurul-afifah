7a65e7b08f01d4d0e28cd6a7c15eefa1ebf7acc6
blacl-domon <jakfar.ilham23@gmail.com>
2024-07-11 20:06:52 +0700

    membuat fitur bot tele dan crud guru

--- arumi/settings.py
+++ arumi/settings.py
@@ -140,3 +140,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TELEGRAM_BOT_TOKEN = '6494449932:AAG1SWc6KPTRu8mniM-qN8PuB6EIxlbFzcA'
--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
+    
+    path('pelanggaran/', views.pelanggaran_view, name='pelanggaran'),
 
     # Utilitas
     path('login', views.entry, name='login'),
@@ -12,10 +14,14 @@ urlpatterns = [
     # Master
     path('', views.home, name='beranda'),
     path('data_santri', views.data, name='data'),
+    
     path('data_guru', views.dataguru, name='dataguru'),
+    path('tambah_data_guru', views.add_dataguru, name='add_dataguru'),
+    path('edit_data_guru/<str:pk>', views.edit_dataguru, name='update_dataguru'),
+    path('delete_data_guru/<str:pk>', views.delete_dataguru, name='delete_dataguru'),
+    
     path('detail_santri/<str:pk>', views.detail_data, name='detail_data'),
     path('tambah_data_santri', views.add_data, name='add_data'),
-    path('tambah_data_guru', views.add_dataguru, name='add_dataguru'),
     path('edit_data_santri<str:pk>', views.update_data, name='update_data'),
     path('data_wilayah', views.wilayah, name='wilayah'),
     path('edit_data_wilayah<str:pk>', views.update_wilayah, name='update_wilayah'),
